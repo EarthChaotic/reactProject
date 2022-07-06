03ee86f21175f9a574cf412b1d5b9b5a83a6a0e2
EarthChaotic <jeraponteesup@gmail.com>
2022-07-06 11:14:06 +0700

    new commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View ,Button } from 'react-native'
 import React from 'react'
 import User from './components/User';
+import MyCustomTextWith from './components/MyCustomTextWith';
+import Count from './components/Count';
+import styles from './components/styles';
+import InputText from './components/InputText';
+import UserNameandPassword from './components/UserNameandPassword';
 
 const App = () => {
   return (
     <View>
-      <User/>
+      {/* <Count/> */}
+     {/* <InputText/> */}
+     <UserNameandPassword/>
     </View>
   )
 }
